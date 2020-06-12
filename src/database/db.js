98768e21962e0f36e48b63b3662a1c6201cac3ca
lucas <lucasmengue96@hotmail.com>
2020-06-12 19:59:03 -0300

    Updated sql insert dynamically in the project

--- src/database/db.js
+++ src/database/db.js
@@ -5,6 +5,7 @@ const sqlite3 = require("sqlite3").verbose();
 // Iniciar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
+module.exports = db;
 // Utilizar o objeto de banco de dados, para nossas operações
 /*
 db.serialize(() => {
